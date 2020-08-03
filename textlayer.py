12485ba3eefa55ec5d81e23bcf634b478d4cce2d
JRyanHall16 <68780684+JRyanHall16@users.noreply.github.com>
2020-08-03 19:07:27 -0400

    Updated textLayer with additional functionality
    Added the ability to select between tiles (which prompts you with an option to accept or choose a different tile) and added some code to create directories for storing the files that are generated.
    Probably also some other stuff, but that's the main things.

--- textlayer.py
+++ textlayer.py
@@ -4,22 +4,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # import time to timestamp filenames so they are automatically sorted and
 # import getpass to conceal password during entry
-import time, getpass
+import time, getpass, re, os
 # constants used for site navigation using XPath
 # XPath seems to be the most direct and efficient way to navigate a website in
 # this manner; however, any change to the website would probably be more likely
 # to break functionality
 siteUrl        = 'https://www.cengage.com'
+outputDir      = 'textLayer\\'
 signInLinkPath = '/html/body/header/div[1]/div[2]/ul/li[3]/a'
 signInUserPath = '//*[@id="idp-discovery-username"]'
 signInNextPath = '//*[@id="idp-discovery-submit"]'
 signInPassPath = '//*[@id="okta-signin-password"]'
 signInButnPath = '//*[@id="okta-signin-submit"]'
-# titleOne is the title that links to the ebook that this
+# tileOne is the tile that links to the ebook that this
 # is intended to download. in the future, functionality could be added to offer
-# a selection of titles instead of only the one static title.
-titleOnePath   = '/html/body/div[2]/div[2]/ng-include/div[1]/main/div[6]/section/ul/li[2]/a/div/div[1]/div[2]/div/button'
-titleNinePath  = '/html/body/div[2]/div[2]/ng-include/div[1]/main/div[6]/section/ul/li[10]/a/div/div[1]/div[2]/div/button'
+# a selection of tiles instead of only the one static tile.
+tilePath       = '/html/body/div[2]/div[2]/ng-include/div[1]/main/div[6]/section/ul/li[#]/a/div/div[1]/div[2]/div/button'
 # this XPath is utilized to standardize the starting page for the script. This
 # links to the first item in the navigation sidebar (presumably the first page)
 firstPagePath  = '/html/body/div/span/ui-view/div/nav/reader-toc/div[2]/ul/li[1]/reader-toc-item/div/div'
@@ -32,6 +32,8 @@ iFramePath     = '//*[@id="iframe-page"]'
 # script used in 'execute_script()' call to click through to the next page
 nextPageScript = 'document.getElementById("next-page").click()'
 pageTitlePath  = '/html/body/div/span/ui-view/div/nav/go-to-page/div/form/input'
+tileTitlePath  = '/html/body/div/span/ui-view/div/div/nav/span[3]'
+tileNamePath   = '/html/body/div[2]/div[2]/ng-include/div[1]/main/div[6]/section/ul/li[#]/a/div/div[2]/p[1]/b'
 # path to the div that contains the content of the page, that is used for the
 # screenshot and is the inspiration for the title of this project
 textLayerPath  = 'html/body/div/div/div/div[2]'
@@ -40,7 +42,9 @@ siteUser  = None
 sitePass  = None
 pageCount = None
 pageStart = None
-userTitle = None
+userTile  = None
+tileName  = None
+tileTitle = None
 # used to login to site (intended specifically for cengage currently but could
 # one day be extended to be used in other situations
 def siteLogin(webdriver, username, password):
@@ -58,11 +62,23 @@ def siteLogin(webdriver, username, password):
 # title
 def getPages(count):
     page = 0
+    try:
+        os.mkdir(os.getcwd()+'\\'+outputDir)
+    except OSError:
+        print('Creation of directory %s failed.' % outputDir)
+    else:
+        print('Creation of directory %s succeeded.' % outputDir)
+    try:
+        os.mkdir(os.getcwd()+'\\'+outputDir+tileTitle)
+    except OSError:
+        print('Creation of directory %s failed.' % tileTitle)
+    else:
+        print('Creation of directory %s succeeded.' % tileTitle)
     while page < count:
         iFrame = driver.find_element_by_xpath(iFramePath)
         pageUrl = iFrame.get_attribute('src')
         pageTitle = driver.find_element_by_xpath(pageTitlePath).get_attribute('value')
-        pageName = str(time.time()).split('.')[0]+pageTitle+'.png'
+        pageName = os.getcwd()+'\\'+outputDir+tileTitle+'\\'+str(time.time()).split('.')[0]+pageTitle+'.png'
         driver.switch_to.window(driver.window_handles[0])
         driver.get(pageUrl)
         textLayer = driver.find_element_by_xpath(textLayerPath)
@@ -95,11 +111,17 @@ def getLoginInfo():
     sitePass = getpass.getpass("Enter your password: ")
 # not sure what this is for haha need to revisit this
 # OH. I remember what this is for but I'm going to leave the line above:
-# the purpose of this section was to extend the title selection once logged in
+# the purpose of this section was to extend the tile selection once logged in
 # however I haven't put any effort into that so far because it's not necessary
-def getTitle():
-    global userTitle
-    userTitle = input("Enter the title number: ")
+def getTile():
+    global userTile, tileName
+    while True:
+        userInput = str(input("Enter the tile number of your selection [1, 2, etc.]: "))
+        userTile = tilePath.split('#')[0]+userInput+tilePath.split('#')[1]
+        userTileName = tileNamePath.split('#')[0]+userInput+tileNamePath.split('#')[1]
+        tileName = driver.find_element_by_xpath(userTileName).get_attribute('textContent')
+        if input('You selected '+tileName+'. Would you like to begin? [y/n] ') == 'y':
+            break
 # ensures that input isn't empty, then sets it to pageStart as a string
 def getPageCount():
     global pageCount, pageStart
@@ -114,17 +136,21 @@ driver = webdriver.Firefox()
 driver.implicitly_wait(30)
 driver.get(siteUrl)
 siteLogin(driver, siteUser, sitePass)
-# getTitle()
-selectedTitle = driver.find_element_by_xpath(titleNinePath)
-selectedTitle.click()
-# once the title is clicked and starts to load, the browser doesn't auto-
+# request a tile selection from user
+getTile()
+selectedTile = driver.find_element_by_xpath(userTile)
+selectedTile.click()
+# once the tile is clicked and starts to load, the browser doesn't auto-
 # matically change focus to the new tab so we do it ourselves
 driver.switch_to.window(driver.window_handles[1])
+tileTitle = driver.find_element_by_xpath(tileTitlePath).get_attribute('textContent')
+re.sub(r'[^\w]', '', tileTitle)
 # then we check to see if we are on the (presumably 'Cover' page and if not
 # click the first element in the nav (this needs to be re-evaluated)
 if driver.find_element_by_xpath(pageTitlePath).get_attribute('value') != 'Cover':
     firstPage = driver.find_element_by_xpath(firstPagePath)
     firstPage.click()
+# get and store title of content
 # this particular loop allows you to grab as many pages as you would like and
 # continues to give you the option to get more as long as you enter explicitly
 # 'y' in response to the input prompt. This is particularly useful if you only
@@ -141,5 +167,5 @@ while True:
     getPages(pageCount)
     if input('Would you like to continue? [y/n] ') != 'y':
         break
-time.sleep(120)
+time.sleep(20)
 driver.quit()
